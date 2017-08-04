fb64e2db12c3af06b568a19dd758b6aac49dfc9a
dengle3 <dengle3@wanda.cn>
2017-08-04 16:16:15 +0800

    add Util module

---
+++ DlUtilsDemo/DlUtils/DLString.swift
@@ -0,0 +1,15 @@
+//
+//  DLString.swift
+//  DlUtilsDemo
+//
+//  Created by 邓乐 on 2017/8/4.
+//  Copyright © 2017年 Wanda. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+   public static func dl() ->String{
+        return "hello world!"
+    }
+}
